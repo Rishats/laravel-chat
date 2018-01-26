9364aad461158aeec115e072cf109bfb42a471e4
Rishats <sultanov-rishat@inbox.ru>
2018-01-26 20:22:44 +0600

    Info from server.js to client added other.

--- ws.server/server.js
+++ ws.server/server.js
@@ -3,7 +3,12 @@ var io = require('socket.io')(6001);
 io.on('connection', function (socket) {
    console.log('New connection',socket.id);
 
+   // Send message
    // socket.send('Message from server');
 
-    socket.emit('server-info', {version: .1});
+    // Fire event
+   // socket.emit('server-info', {version: .1});
+
+    // Send data about new user to people on the site(in console);
+   socket.broadcast.send('New user');
 });
